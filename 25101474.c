eff76befd507567dcd2ddb358da186135f163e51
Daniel Gama <danielgmbr30@gmail.com>
2026-06-10 17:01:24 -0300

    feat: funcao com dois operandos

--- 25101474.c
+++ 25101474.c
@@ -3,11 +3,27 @@
 #include <string.h>
 #include <math.h>
 
-static int verifica_Operador_Basico(char *token) {
+#define PI 3.14159265358979323846
+
+static int verifica_Operador(char *token) {
     return strcmp(token, "+") == 0 ||
            strcmp(token, "-") == 0 ||
            strcmp(token, "*") == 0 ||
-           strcmp(token, "/") == 0;
+           strcmp(token, "/") == 0 ||
+           strcmp(token, "%") == 0 ||
+           strcmp(token, "^") == 0;
+}
+
+static int verifica_Funcao(char *token) {
+    return strcmp(token, "log") == 0 ||
+           strcmp(token, "raiz") == 0 ||
+           strcmp(token, "sen") == 0 ||
+           strcmp(token, "cos") == 0 ||
+           strcmp(token, "tg") == 0;
+}
+
+static float grausParaRadianos(float graus) { //usar esse pq o math nao aceita graus literalmente
+    return graus * PI / 180.0;
 }
 
 float getValor(char *Str) {
@@ -15,7 +31,10 @@ float getValor(char *Str) {
     int topo = -1;
 
     char copia[512];
-    strcpy(copia, Str);
+
+    /*evita overflow limitando o tamanho a ate 511*/
+    strncpy(copia, Str, 511);
+    copia[511] = '\0';
 
     char *token = strtok(copia, " ");
 
@@ -27,7 +46,7 @@ float getValor(char *Str) {
             topo++;
             pilha[topo] = numero;
         }
-        else if (verifica_Operador_Basico(token)) {
+        else if (verifica_Operador(token)) { //precisa de dois operandos
             if (topo < 1) {
                 return NAN;
             }
@@ -52,10 +71,57 @@ float getValor(char *Str) {
 
                 resultado = a / b;
             }
+            else if (strcmp(token, "%") == 0) {
+                if (b == 0) {
+                    return NAN;
+                }
+
+                resultado = fmod(a, b);
+            }
+            else if (strcmp(token, "^") == 0) {
+                resultado = pow(a, b);
+            }
 
             topo++;
             pilha[topo] = resultado;
         }
+
+        else if (verifica_Funcao(token)) { //precisa de um operando
+            if (topo < 0) {
+                return NAN;
+            }
+
+            float a = pilha[topo--];
+            float resultado = 0;
+
+            if (strcmp(token, "log") == 0) {
+                if (a <= 0) {
+                    return NAN;
+                }
+
+                resultado = log10(a);
+            }
+            else if (strcmp(token, "raiz") == 0) {
+                if (a < 0) {
+                    return NAN;
+                }
+
+                resultado = sqrt(a);
+            }
+            else if (strcmp(token, "sen") == 0) {
+                resultado = sin(grausParaRadianos(a));
+            }
+            else if (strcmp(token, "cos") == 0) {
+                resultado = cos(grausParaRadianos(a));
+            }
+            else if (strcmp(token, "tg") == 0) {
+                resultado = tan(grausParaRadianos(a));
+            }
+
+            topo++;
+            pilha[topo] = resultado;
+        }
+
         else {
             return NAN;
         }
@@ -68,4 +134,71 @@ float getValor(char *Str) {
     }
 
     return pilha[topo];
+}
+
+char *getInFixa(char *Str) {
+    static char resultadoFinal[512];
+
+    char pilha[512][512];
+    int topo = -1;
+
+    char copia[512];
+    strncpy(copia, Str, 511);
+    copia[511] = '\0';
+
+    char *token = strtok(copia, " ");
+
+    while (token != NULL) {
+        char *fim;
+        strtof(token, &fim);
+
+        if (*fim == '\0') {
+            topo++;
+            strcpy(pilha[topo], token);
+        }
+        else if (verifica_Operador(token)) {
+            if (topo < 1) {
+                return NULL;
+            }
+
+            char b[512];
+            char a[512];
+            char resultado[512];
+
+            strcpy(b, pilha[topo--]);
+            strcpy(a, pilha[topo--]);
+
+            snprintf(resultado, sizeof(resultado), "(%s%s%s)", a, token, b);
+
+            topo++;
+            strcpy(pilha[topo], resultado);
+        }
+        else if (verifica_Funcao(token)) {
+            if (topo < 0) {
+                return NULL;
+            }
+
+            char a[512];
+            char resultado[512];
+
+            strcpy(a, pilha[topo--]);
+
+            snprintf(resultado, sizeof(resultado), "%s(%s)", token, a);
+
+            topo++;
+            strcpy(pilha[topo], resultado);
+        }
+        else {
+            return NULL;
+        }
+
+        token = strtok(NULL, " ");
+    }
+
+    if (topo != 0) {
+        return NULL;
+    }
+
+    strcpy(resultadoFinal, pilha[topo]);
+    return resultadoFinal;
 }
--- main.c
+++ main.c
@@ -4,10 +4,11 @@
 float getValor(char *Str);
 
 int main() {
-    printf("%.2f\n", getValor("3 4 +"));
-    printf("%.2f\n", getValor("3 4 + 5 *"));
-    printf("%.2f\n", getValor("7 2 * 4 +"));
-    printf("%.2f\n", getValor("6 2 / 3 + 4 *"));
+    printf("%.3f\n", getValor("2 3 + log 5 /"));
+    printf("%.3f\n", getValor("10 log 3 ^ 2 +"));
+    printf("%.3f\n", getValor("45 60 + 30 cos *"));
+    printf("%.3f\n", getValor("0.5 45 sen 2 ^ +"));
+    printf("%.3f\n", getValor("9 raiz"));
 
     return 0;
 }
